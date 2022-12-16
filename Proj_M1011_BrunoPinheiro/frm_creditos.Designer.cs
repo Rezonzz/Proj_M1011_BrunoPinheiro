22f2a7cfae8244ede4f9fcdbe88e6c76011c423f
Bruno Pinheiro <95320065+Rezonzz@users.noreply.github.com>
2022-12-16 17:24:35 +0000

    Proj

---
+++ Proj_M1011_BrunoPinheiro/frm_creditos.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace Proj_M1011_BrunoPinheiro
+{
+    partial class frm_creditos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_creditos));
+            this.tmr_creditos = new System.Windows.Forms.Timer(this.components);
+            this.lbl_nome5 = new System.Windows.Forms.Label();
+            this.lbl_p = new System.Windows.Forms.Label();
+            this.lbl_nome4 = new System.Windows.Forms.Label();
+            this.lbl_d = new System.Windows.Forms.Label();
+            this.lbl_nome6 = new System.Windows.Forms.Label();
+            this.lbl_dep = new System.Windows.Forms.Label();
+            this.lbl_nome3 = new System.Windows.Forms.Label();
+            this.lbl_ts = new System.Windows.Forms.Label();
+            this.lbl_nome2 = new System.Windows.Forms.Label();
+            this.lbl_ap = new System.Windows.Forms.Label();
+            this.lbl_nome1 = new System.Windows.Forms.Label();
+            this.lbl_dp = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tmr_creditos
+            // 
+            this.tmr_creditos.Enabled = true;
+            this.tmr_creditos.Interval = 50;
+            this.tmr_creditos.Tick += new System.EventHandler(this.tmr_creditos_Tick);
+            // 
+            // lbl_nome5
+            // 
+            this.lbl_nome5.AutoSize = true;
+            this.lbl_nome5.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_nome5.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lbl_nome5.Location = new System.Drawing.Point(104, 373);
+            this.lbl_nome5.Name = "lbl_nome5";
+            this.lbl_nome5.Size = new System.Drawing.Size(113, 21);
+            this.lbl_nome5.TabIndex = 94;
+            this.lbl_nome5.Text = "Bruno Pinheiro";
+            this.lbl_nome5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_p
+            // 
+            this.lbl_p.AutoSize = true;
+            this.lbl_p.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_p.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lbl_p.Location = new System.Drawing.Point(110, 337);
+            this.lbl_p.Name = "lbl_p";
+            this.lbl_p.Size = new System.Drawing.Size(101, 27);
+            this.lbl_p.TabIndex = 93;
+            this.lbl_p.Text = "Patrocinio";
+            this.lbl_p.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_nome4
+            // 
+            this.lbl_nome4.AutoSize = true;
+            this.lbl_nome4.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_nome4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lbl_nome4.Location = new System.Drawing.Point(105, 299);
+            this.lbl_nome4.Name = "lbl_nome4";
+            this.lbl_nome4.Size = new System.Drawing.Size(113, 21);
+            this.lbl_nome4.TabIndex = 92;
+            this.lbl_nome4.Text = "Bruno Pinheiro";
+            this.lbl_nome4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_d
+            // 
+            this.lbl_d.AutoSize = true;
+            this.lbl_d.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_d.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lbl_d.Location = new System.Drawing.Point(117, 269);
+            this.lbl_d.Name = "lbl_d";
+            this.lbl_d.Size = new System.Drawing.Size(82, 27);
+            this.lbl_d.TabIndex = 91;
+            this.lbl_d.Text = "Direção";
+            this.lbl_d.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_nome6
+            // 
+            this.lbl_nome6.AutoSize = true;
+            this.lbl_nome6.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_nome6.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lbl_nome6.Location = new System.Drawing.Point(104, 446);
+            this.lbl_nome6.Name = "lbl_nome6";
+            this.lbl_nome6.Size = new System.Drawing.Size(113, 21);
+            this.lbl_nome6.TabIndex = 90;
+            this.lbl_nome6.Text = "Bruno Pinheiro";
+            this.lbl_nome6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_dep
+            // 
+            this.lbl_dep.AutoSize = true;
+            this.lbl_dep.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_dep.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lbl_dep.Location = new System.Drawing.Point(81, 412);
+            this.lbl_dep.Name = "lbl_dep";
+            this.lbl_dep.Size = new System.Drawing.Size(163, 27);
+            this.lbl_dep.TabIndex = 89;
+            this.lbl_dep.Text = "Desenvolvido por";
+            this.lbl_dep.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_nome3
+            // 
+            this.lbl_nome3.AutoSize = true;
+            this.lbl_nome3.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_nome3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lbl_nome3.Location = new System.Drawing.Point(105, 224);
+            this.lbl_nome3.Name = "lbl_nome3";
+            this.lbl_nome3.Size = new System.Drawing.Size(113, 21);
+            this.lbl_nome3.TabIndex = 88;
+            this.lbl_nome3.Text = "Bruno Pinheiro";
+            this.lbl_nome3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_ts
+            // 
+            this.lbl_ts.AutoSize = true;
+            this.lbl_ts.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_ts.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lbl_ts.Location = new System.Drawing.Point(81, 194);
+            this.lbl_ts.Name = "lbl_ts";
+            this.lbl_ts.Size = new System.Drawing.Size(157, 27);
+            this.lbl_ts.TabIndex = 87;
+            this.lbl_ts.Text = "Técnico de Som";
+            this.lbl_ts.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_nome2
+            // 
+            this.lbl_nome2.AutoSize = true;
+            this.lbl_nome2.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_nome2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lbl_nome2.Location = new System.Drawing.Point(106, 152);
+            this.lbl_nome2.Name = "lbl_nome2";
+            this.lbl_nome2.Size = new System.Drawing.Size(113, 21);
+            this.lbl_nome2.TabIndex = 86;
+            this.lbl_nome2.Text = "Bruno Pinheiro";
+            this.lbl_nome2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_ap
+            // 
+            this.lbl_ap.AutoSize = true;
+            this.lbl_ap.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_ap.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lbl_ap.Location = new System.Drawing.Point(37, 123);
+            this.lbl_ap.Name = "lbl_ap";
+            this.lbl_ap.Size = new System.Drawing.Size(262, 27);
+            this.lbl_ap.TabIndex = 85;
+            this.lbl_ap.Text = "Assistente de Programação";
+            this.lbl_ap.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_nome1
+            // 
+            this.lbl_nome1.AutoSize = true;
+            this.lbl_nome1.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_nome1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lbl_nome1.Location = new System.Drawing.Point(106, 80);
+            this.lbl_nome1.Name = "lbl_nome1";
+            this.lbl_nome1.Size = new System.Drawing.Size(113, 21);
+            this.lbl_nome1.TabIndex = 84;
+            this.lbl_nome1.Text = "Bruno Pinheiro";
+            this.lbl_nome1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_dp
+            // 
+            this.lbl_dp.AutoSize = true;
+            this.lbl_dp.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_dp.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lbl_dp.Location = new System.Drawing.Point(60, 48);
+            this.lbl_dp.Name = "lbl_dp";
+            this.lbl_dp.Size = new System.Drawing.Size(214, 27);
+            this.lbl_dp.TabIndex = 83;
+            this.lbl_dp.Text = "Design e Programação";
+            this.lbl_dp.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // frm_creditos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(337, 514);
+            this.Controls.Add(this.lbl_nome5);
+            this.Controls.Add(this.lbl_p);
+            this.Controls.Add(this.lbl_nome4);
+            this.Controls.Add(this.lbl_d);
+            this.Controls.Add(this.lbl_nome6);
+            this.Controls.Add(this.lbl_dep);
+            this.Controls.Add(this.lbl_nome3);
+            this.Controls.Add(this.lbl_ts);
+            this.Controls.Add(this.lbl_nome2);
+            this.Controls.Add(this.lbl_ap);
+            this.Controls.Add(this.lbl_nome1);
+            this.Controls.Add(this.lbl_dp);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frm_creditos";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Jordan - Créditos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer tmr_creditos;
+        private System.Windows.Forms.Label lbl_nome5;
+        private System.Windows.Forms.Label lbl_p;
+        private System.Windows.Forms.Label lbl_nome4;
+        private System.Windows.Forms.Label lbl_d;
+        private System.Windows.Forms.Label lbl_nome6;
+        private System.Windows.Forms.Label lbl_dep;
+        private System.Windows.Forms.Label lbl_nome3;
+        private System.Windows.Forms.Label lbl_ts;
+        private System.Windows.Forms.Label lbl_nome2;
+        private System.Windows.Forms.Label lbl_ap;
+        private System.Windows.Forms.Label lbl_nome1;
+        private System.Windows.Forms.Label lbl_dp;
+    }
+}
---
+++ Proj_M1011_BrunoPinheiro/frm_creditos.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proj_M1011_BrunoPinheiro
+{
+    public partial class frm_creditos : Form
+    {
+        public frm_creditos()
+        {
+            InitializeComponent();
+        }
+
+        private void tmr_creditos_Tick(object sender, EventArgs e)
+        {
+            lbl_dp.Location = new Point(lbl_dp.Location.X, lbl_dp.Location.Y - 2);
+            lbl_nome1.Location = new Point(lbl_nome1.Location.X, lbl_nome1.Location.Y - 2);
+            lbl_ap.Location = new Point(lbl_ap.Location.X, lbl_ap.Location.Y - 2);
+            lbl_nome2.Location = new Point(lbl_nome2.Location.X, lbl_nome2.Location.Y - 2);
+            lbl_ts.Location = new Point(lbl_ts.Location.X, lbl_ts.Location.Y - 2);
+            lbl_nome3.Location = new Point(lbl_nome3.Location.X, lbl_nome3.Location.Y - 2);
+            lbl_d.Location = new Point(lbl_d.Location.X, lbl_d.Location.Y - 2);
+            lbl_nome4.Location = new Point(lbl_nome4.Location.X, lbl_nome4.Location.Y - 2);
+            lbl_p.Location = new Point(lbl_p.Location.X, lbl_p.Location.Y - 2);
+            lbl_nome5.Location = new Point(lbl_nome5.Location.X, lbl_nome5.Location.Y - 2);
+            lbl_dep.Location = new Point(lbl_dep.Location.X, lbl_dep.Location.Y - 2);
+            lbl_nome6.Location = new Point(lbl_nome6.Location.X, lbl_nome6.Location.Y - 2);
+
+            if (lbl_nome6.Location.Y <= 4)
+            {
+                lbl_dp.Location = new Point(61, 557);
+                lbl_nome1.Location = new Point(107, 589);
+                lbl_ap.Location = new Point(38, 632);
+                lbl_nome2.Location = new Point(107, 661);
+                lbl_ts.Location = new Point(82, 703);
+                lbl_nome3.Location = new Point(106, 733);
+                lbl_d.Location = new Point(118, 778);
+                lbl_nome4.Location = new Point(106, 808);
+                lbl_p.Location = new Point(111, 846);
+                lbl_nome5.Location = new Point(105, 882);
+                lbl_dep.Location = new Point(82, 921);
+                lbl_nome6.Location = new Point(105, 955);
+            }
+        }
+    }
+}
