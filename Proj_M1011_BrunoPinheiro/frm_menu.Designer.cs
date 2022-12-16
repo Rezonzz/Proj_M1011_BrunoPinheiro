1c34a1bd52ffb05ede50d0cee844ae37cbc65de9
Bruno Pinheiro <95320065+Rezonzz@users.noreply.github.com>
2022-12-16 15:07:30 +0000

    Proj

--- Proj_M1011_BrunoPinheiro/frm_menu.Designer.cs
+++ Proj_M1011_BrunoPinheiro/frm_menu.Designer.cs
@@ -35,6 +35,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.ClientSize = new System.Drawing.Size(634, 341);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
